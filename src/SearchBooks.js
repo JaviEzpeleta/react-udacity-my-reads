9861bac9d4de989a5ac34f7eeaf3ed4386f9f468
Javier Ezpeleta <javierezpeleta@gmail.com>
2017-07-11 02:09:55 +0200

    [feature] not sure if this is the best way to do it, but now you'll be able to see the "loading gif" while the search request to the server is being processed, and it will disappear once the results are back

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,23 +2,37 @@ import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
+import Loading from './utils/Loading'
 
 
 class SearchBooks extends Component {
 
 	state = { searchResults: []}
 
+	showLoading() {
+		this.setState({ isLoading: true })
+	}
+
+	hideLoading() {
+		this.setState({ isLoading: false })
+	}
+
+
 	searchBooks = (query) => {
+		this.showLoading()
+		let searchResults = []
 		if (query.length) {
-			this.setState({ isLoading: true })
+
 			BooksAPI.search(query).then(results => {
-				let searchResults = results.map(result => {
-					return this.props.books.has(result.id) ? this.props.books.get(result.id) : result;
-				})
+				if (results.length > 0) {
+					searchResults = results.map(result => {
+						return this.props.books.has(result.id) ? this.props.books.get(result.id) : result;
+					})
+				}
 				this.setState({
 					searchResults: searchResults,
-					isLoading: false
 				})
+				this.hideLoading()
 			})
 		} else {
 			this.setState({searchResults: []})
@@ -27,7 +41,11 @@ class SearchBooks extends Component {
 
 	render() {
 		return (
+
 			<div className="search-books">
+
+	        {this.state.isLoading && ( <Loading/> )}
+
 				<div className="search-books-bar">
 					<Link className="close-search" to="/">Close</Link>
 					<div className="search-books-input-wrapper">

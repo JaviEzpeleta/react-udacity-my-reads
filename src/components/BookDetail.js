adade7810cc56fd790a194917d33e164ef66a4c5
Javier Ezpeleta <javierezpeleta@gmail.com>
2017-07-19 01:04:05 +0200

    Formatted a Book Detail page, decent, at least enough for me to move on. It was useful: it helped me start using "Moment" for the first time in my life! :D

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import * as BooksAPI from './../BooksAPI'
 import BookDetailBar from './BookDetailBar'
 import Loading from './../utils/Loading'
+import Moment from 'react-moment';
 
 class BookDetail extends Component {
 
@@ -26,7 +27,7 @@ class BookDetail extends Component {
 		const { book, isLoading } = this.state;
 		const { imageLinks } = book;
 		let image = imageLinks ? imageLinks.thumbnail : 'https://books.google.com/googlebooks/images/no_cover_thumb.gif';
-
+		image = image.replace("http://", "https://");
 
 		return (
 			<div>
@@ -36,7 +37,45 @@ class BookDetail extends Component {
 					<div>
 			        	<BookDetailBar title={this.state.book.title} />
 			        	<div className="book-detail-body">
-							<div className="book-cover" style={{backgroundImage: 'url('+image+')' }} />
+							<div className="book-detail-cover" style={{backgroundImage: 'url('+image+')' }} />
+				        	<div className="book-detail-subbody">
+								<div className="book-detail-label"> Title: </div>
+					        	<div className="book-detail-title">
+					        		{book.title}
+				        		</div>
+				        		<div className="book-detail-authors">
+									{ book.authors && (
+										<div className="book-detail-label"> Authors: </div>
+									) }
+									{ book.authors && book.authors.map( (author, index) => (
+										<div key={index}>
+											{author}
+										</div>
+										)
+									) }
+				        		</div>
+								<div className="book-detail-label"> Publisher: </div>
+					        	<div className="book-detail-publisher">
+					        		{book.publisher}
+				        		</div>
+				        		<div className="book-detail-categories">
+									{ book.authors && (
+										<div className="book-detail-label"> Categories: </div>
+									) }
+									{ book.categories && book.categories.map( (category, index) => (
+										<div key={index}>
+											{category}
+										</div>
+										)
+									) }
+				        		</div>
+
+								<div className="book-detail-label"> Date published: </div>
+					        	<div className="book-detail-publisher">
+					        		<Moment format="YYYY, MMM Do">{book.publishedDate}</Moment>
+				        		</div>
+
+				        	</div>
 			        	</div>
 			        </div>
 				)}
